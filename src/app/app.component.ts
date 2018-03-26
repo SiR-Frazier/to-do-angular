ca9c9d4a3409123364f1b4b156541d07cd2c007c
Bryce Frazier and Nico Daunt <bryce@>
2018-03-26 10:26:30 -0700

    initialize git repo, set up typescript/node foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'test-app-selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() +1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish angular hw'),
+    new Task('Javascript brainstorm'),
+    new Task('Add readme')
+
+  ];
+
+
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
